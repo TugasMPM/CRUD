506567a9c5da57b9059d7ed89a2bb67734bacc74
Nadhif Lazuardi <berkasnadhif@gmail.com>
2023-10-05 03:13:30 +0700

    Add Undip logo as background

--- application/views/home.php
+++ application/views/home.php
@@ -3,15 +3,24 @@
  
 <head>
     <?php $this->load->view('_partials/head.php'); ?>
+    <style>
+    body {
+        background-image: url('https://1.bp.blogspot.com/-Vc0FBbtFxII/VlRtXr1IvpI/AAAAAAAABH8/DJgtRUlxaVY/s1600/Logo%2BUndip%2B%2528Black%2Band%2BWhite%2529%2B%2528JPG%2529.jpg');
+        background-repeat: no-repeat;
+        background-size: contain;
+        background-position: center 200px;
+    }
+  </style>
+
 </head>
  
 <body>
     <?php $this->load->view('_partials/navbar.php');
     ?>
-    <h1>Home Page</h1>
+    <h1 style="margin-left: 30px; margin-top: 20px">Home Page</h1>
     <div class="container">
-    <h1>Home Page</h1>
-</div>
+    
+    </div>
     
  
     <?php $this->load->view('_partials/footer.php'); ?>
